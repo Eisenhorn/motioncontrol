88773bd372bd98cba938f86e77df61d7a800c955
Eisen Horn <Mario@.(none)>
2011-09-01 23:56:53 +0200

    template project

---
+++ src/motioncontrol/eisenhorn/dnsdojo/com/MotioncontrolActivity.java
@@ -0,0 +1,13 @@
+package motioncontrol.eisenhorn.dnsdojo.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MotioncontrolActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
